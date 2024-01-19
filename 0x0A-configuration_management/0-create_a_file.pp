92c86ca62937c9efcc2ea567758838fc694d5b8f
Younrag <younes.raguebi@usmba.ac.ma>
2024-01-19 14:12:01 +0100

    Done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # create a file in /tmp/
+
 file { 'temporary file':
   ensure  => file,
-  path    => '/tmp/holberton',
+  path    => '/tmp/school',
   mode    => '0744',
   group   => 'www-data',
   owner   => 'www-data',
