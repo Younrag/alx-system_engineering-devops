f7e2984ed2c0c541379b4af1102d98dd8ccfbe9b
Younrag <younes.raguebi@usmba.ac.ma>
2024-02-19 21:38:49 +0100

    Done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,27 +2,28 @@
 """data from API"""
 
 import requests
-from sys import argv
+import sys
 
 
 if __name__ == "__main__":
-    employeeRequest = requests.get(
-        "https://jsonplaceholder.typicode.com/users/" + argv[1])
-    employeeDict = employeeRequest.json()
+    # Base URL for the JSONPlaceholder API
+    url = "https://jsonplaceholder.typicode.com/"
 
-    todoRequest = requests.get(
-        "https://jsonplaceholder.typicode.com/todos?userId=" + argv[1])
-    todoList = todoRequest.json()
+    # Get the employee information using the provided employee ID
+    employee_id = sys.argv[1]
+    user = requests.get(url + "users/{}".format(employee_id)).json()
 
-    employeeName = employeeDict.get("name")
-    numberOfTasks = len(todoList)
-    completedTasks = [
-        dict for dict in todoList if dict.get("completed") is True]
+    # Get the to-do list for the employee using the provided employee ID
+    params = {"userId": employee_id}
+    todos = requests.get(url + "todos", params).json()
 
-    print('Employee {} is done with tasks({}/{}):'.format(
-        employeeName,
-        len(completedTasks),
-        numberOfTasks))
+    # Filter completed tasks and count them
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
 
-    for completeTask in completedTasks:
-        print("\t {}".format(completeTask.get("title")))
+    # Print the employee's name and the number of completed tasks
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
+
+    # Print the completed tasks one by one with indentation
+    [print("\t {}".format(complete)) for complete in completed]
+
