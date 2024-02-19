8946dc52041786c5fc2afbd5eb5ef1661f00a4e7
Younrag <younes.raguebi@usmba.ac.ma>
2024-02-19 21:35:20 +0100

    Done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-'''
-data from API
-'''
+"""data from API"""
 
-import re
 import requests
-import sys
+from sys import argv
 
-REST_API = "https://jsonplaceholder.typicode.com"
 
-if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        if re.fullmatch(r'\d+', sys.argv[1]):
-            id = int(sys.argv[1])
-            req = requests.get('{}/users/{}'.format(REST_API, id)).json()
-            task_req = requests.get('{}/todos'.format(REST_API)).json()
-            emp_name = req.get('name')
-            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
-            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-            print(
-                'Employee {} is done with tasks({}/{}):'.format(
-                    emp_name,
-                    len(completed_tasks),
-                    len(tasks)
-                )
-            )
-            if len(completed_tasks) > 0:
-                for task in completed_tasks:
-                    print('\t {}'.format(task.get('title')))
+if __name__ == "__main__":
+    employeeRequest = requests.get(
+        "https://jsonplaceholder.typicode.com/users/" + argv[1])
+    employeeDict = employeeRequest.json()
+
+    todoRequest = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId=" + argv[1])
+    todoList = todoRequest.json()
+
+    employeeName = employeeDict.get("name")
+    numberOfTasks = len(todoList)
+    completedTasks = [
+        dict for dict in todoList if dict.get("completed") is True]
+
+    print('Employee {} is done with tasks({}/{}):'.format(
+        employeeName,
+        len(completedTasks),
+        numberOfTasks))
+
+    for completeTask in completedTasks:
+        print("\t {}".format(completeTask.get("title")))i
