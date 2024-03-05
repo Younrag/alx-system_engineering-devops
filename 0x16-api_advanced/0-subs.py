c88caed7b584f21e1e0a47d9233fa43de101dfae
Younrag <younes.raguebi@usmba.ac.ma>
2024-03-05 21:26:38 +0100

    Done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,8 +7,8 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Number of subscribers for a given subreddit."""
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/about.json'
+    user = {"User-Agent": "chrome"}
+    url = requests.get("https://www.reddit.com/r/{}/about.json"
                        .format(subreddit), headers=user).json()
     try:
         return url.get('data').get('subscribers')
