091f588c822ab69573f93ed57bce28e3399d5287
Younrag <younes.raguebi@usmba.ac.ma>
2024-03-05 21:23:23 +0100

    Done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,12 +3,10 @@
 
 
 import requests
-from sys import argv
 
 
 def number_of_subscribers(subreddit):
-    """Number of subscribers for a given subreddit.
-    """
+    """Number of subscribers for a given subreddit."""
     user = {'User-Agent': 'Lizzie'}
     url = requests.get('https://www.reddit.com/r/{}/about.json'
                        .format(subreddit), headers=user).json()
@@ -16,7 +14,3 @@ def number_of_subscribers(subreddit):
         return url.get('data').get('subscribers')
     except Exception:
         return 0
-
-
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
